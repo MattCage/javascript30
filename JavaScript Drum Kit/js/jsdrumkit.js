8f0734547a3ac34ebf4f37d8ec238c733d292ddb
Mattia Bondrano <mattcage89@gmail.com>
2017-05-01 17:49:41 +0200

    JS Drum Kit: Added some basic style and commented the JS code

--- JavaScript Drum Kit/js/jsdrumkit.js
+++ JavaScript Drum Kit/js/jsdrumkit.js
@@ -1,9 +1,12 @@
+// Window will listen to any key down
 window.addEventListener('keydown', function(e) {
+  // Feed up a constant with the element selected by the keycode of the key pressed
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log(audio);
-  if (!audio) {
-    return;
-  }
+  // If the audio constant does not exist, return from the function
+  if (!audio) return;
+
+  // Restart the sound and play it
   audio.currentTime = 0;
   audio.play();
+  
 });
