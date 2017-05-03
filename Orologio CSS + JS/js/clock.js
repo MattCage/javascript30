59e7bc042cafc9c6501765bd994e08a133654e5b
Mattia Bondrano <mattcage89@gmail.com>
2017-05-03 20:06:35 +0200

    Orologio CCS + JS: extended JS logic to minutes and hours hands

--- Orologio CSS + JS/js/clock.js
+++ Orologio CSS + JS/js/clock.js
@@ -1,11 +1,19 @@
 var secondsHand = document.querySelector('div.hand.seconds');
+var minutesHand = document.querySelector('div.hand.minutes');
+var hoursHand = document.querySelector('div.hand.hours');
 
 function setDate() {
   var now = new Date();
   var seconds = now.getSeconds();
-  var secondsInDegrees = ((seconds / 60) * 360);
+  var secondsInDegrees = ((seconds / 60) * 360) + 90;
+  var minutes = now.getMinutes();
+  var minutesInDegrees = ((minutes / 60) * 360) + 90;
+  var hours = now.getHours();
+  var hoursInDegrees = ((hours / 24) * 360) + 90;
 
   secondsHand.style.transform = "rotate(" + secondsInDegrees + "deg)";
+  minutesHand.style.transform = "rotate(" + minutesInDegrees + "deg)";
+  hoursHand.style.transform = "rotate(" + hoursInDegrees + "deg)";
 
   console.log(seconds);
 
